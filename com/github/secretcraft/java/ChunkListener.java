3ea421ea79ec2c1f7cb6a3fc9eb63c0b632f394d
sirkarpfen <stephan.scholz@gmx.at>
2012-07-15 00:36:55 +0300

    small changes

--- com/github/secretcraft/java/ChunkListener.java
+++ com/github/secretcraft/java/ChunkListener.java
@@ -1,5 +1,6 @@
 package com.github.secretcraft.java;
 
+
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,47 +15,47 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkUnloadEvent;
 
 public class ChunkListener implements Listener {
-	
+
 	private int animalsPerChunk;
 	private List<Animals> animals = new LinkedList<Animals>();
-	
+
 	public ChunkListener(Configuration config) {
 		animalsPerChunk = config.getInt("animals-per-chunk");
 	}
-	
+
 	@EventHandler
 	public void onChunkUnload(ChunkUnloadEvent event) {
-		
+
 		Entity[] chunkEntities = event.getChunk().getEntities();
-		
-		for(int i = 0; i < chunkEntities.length; i++) {
-			
-			if(chunkEntities[i] instanceof Animals) {
-				animals.add((Animals)chunkEntities[i]);
+
+		for(Entity entity : chunkEntities) {
+
+			if(entity instanceof Animals) {
+				animals.add((Animals)entity);
 			} else {
-				if(chunkEntities[i] instanceof Monster) {
-					Monster monster = (Monster)chunkEntities[i];
-					monster.setHealth(0);
+				if(entity instanceof Monster) {
+					Monster monster = (Monster)entity;
+					monster.remove();
 				}
-				if(chunkEntities[i] instanceof Slime) {
-					Slime slime = (Slime)chunkEntities[i];
-					slime.setHealth(0);
+				if(entity instanceof Slime) {
+					Slime slime = (Slime)entity;
+					slime.remove();
 				}
-				if(chunkEntities[i] instanceof Ghast) {
-					Ghast ghast = (Ghast)chunkEntities[i];
-					ghast.setHealth(0);
+				if(entity instanceof Ghast) {
+					Ghast ghast = (Ghast)entity;
+					ghast.remove();
 				}
 			}
 		}
-		
+
 		if(animals.size() > animalsPerChunk) {
 			int max = animals.size()-animalsPerChunk;
 			for(int i = 0; i < max; i++) {
 				Animals a = animals.get(i);
-				a.setHealth(0);
+				a.remove();
 			}
 		}
-		
+
 	}
-	
+
 }
